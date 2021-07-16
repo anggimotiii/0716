ba5fdb8deb4abcf93ecd45109902f7d22da95e57
osk1105 <osk1105@DESKTOP-NCOH71S>
2021-07-16 10:38:10 +0900

    PizzaExam 업로드

---
+++ PizzaExam/src/PizzaMain.java
@@ -0,0 +1,12 @@
+
+public class PizzaMain {
+
+	public static void main(String[] args) {
+		Pizza Pizza1 = new Pizza(20,"∆‰∆€∑Œ¥œ");
+		Pizza Pizza2 = new Pizza(18, "Ω¥∆€Ω¥«¡∏≤");
+		
+		System.out.println(Pizza1.toString());
+		System.out.println(Pizza2.toString());
+	}
+
+}
